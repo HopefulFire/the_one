fc69a3f1dbc82dd112e4485dfd1120424b122a69
Jade Lynn Masker <jadelynn@masker.family>
2020-11-26 16:12:44 -0500

    add bcrypt, add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  validates :email, :nickname, presence: true
+  validates :uid, presence: true, uniqueness: true, if: :omniauth?
+  validates :password, presence: true, uniqueness: true, unless: :omniauth?
+
+  has_secure_password
+
+  private
+
+  def omniauth?
+    if uid
+      true
+    else
+      false
+    end
+  end
 end
