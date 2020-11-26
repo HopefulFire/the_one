2c45ed6c3d0394cddc5a6dc1d6fc3ceaeff460db
Jade Lynn Masker <jadelynn@masker.family>
2020-11-26 17:31:44 -0500

    some controller work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,33 @@
 class UsersController < ApplicationController
-  def show
-  end
+  before_action :find_user, only: %i[show edit update destroy]
+
+  def show; end
 
   def index
+    @users = User.order('name asc')
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    user = User.create(user_params)
+    redirect_to user_path(user)
   end
 
-  def edit
+  def edit; end
+
+  def update
   end
 
   def destroy
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).allow(:name, :email, :password, :uid)
   end
 end
