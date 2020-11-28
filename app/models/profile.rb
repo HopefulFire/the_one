7693d542ce9632aca6f5b7da66102e23989264d4
Jade Lynn Masker <jadelynn@masker.family>
2020-11-27 20:54:58 -0500

    remove gender silliness

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,14 +3,4 @@ class Profile < ApplicationRecord
   validates :likes, :dislikes, :gender, :sex_pref, presence: true
 
   belongs_to :user
-
-  def gender
-    if is_male
-      'Male'
-    elsif is_male == false
-      'Female'
-    else
-      'Non-Binary'
-    end
-  end
 end
