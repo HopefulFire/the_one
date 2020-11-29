beb30762f0e26dafaa7fc273082db13dd27834c4
Jade Lynn Masker <jadelynn@masker.family>
2020-11-29 13:28:20 -0500

    rails g model Request user_id:integer match_id:integer message:text blocked:boolean

---
+++ db/migrate/20201129182643_create_requests.rb
@@ -0,0 +1,12 @@
+class CreateRequests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :requests do |t|
+      t.integer :user_id
+      t.integer :match_id
+      t.text :message
+      t.boolean :blocked
+
+      t.timestamps
+    end
+  end
+end
