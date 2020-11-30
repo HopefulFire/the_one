fc4787b9604a40963580e17f0010249eb0381ce4
Jade Lynn Masker <jadelynn@masker.family>
2020-11-29 21:15:54 -0500

    start fresh

--- app/models/profile.rb
+++
@@ -1,5 +0,0 @@
-class Profile < ApplicationRecord
-  validates :bio, :likes, :dislikes, :gender, :sex_pref, presence: true
-
-  belongs_to :user
-end
--- db/migrate/20201127225446_add_foreign_key_to_profile.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToProfile < ActiveRecord::Migration[6.0]
-  def change
-    add_column :profiles, :user_id, :integer
-  end
-end
