7c05ebf67a9295dc396004b13dbe8ff238ed6c8a
Jade Lynn Masker <jadelynn@masker.family>
2020-11-30 20:28:34 -0500

    work on users controller, validations, and github authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,29 @@
 class UsersController < ApplicationController
-  def show
-  end
+  before_action :find_user, only: %i[show edit update]
+
+  def show; end
 
   def index
+    @users = User.order(nickname: :asc)
   end
 
   def new
+    @user = User.new
   end
 
-  def edit
+  def create
+
+  end
+
+  def edit; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :uid)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   has_many :received_messages, class_name: :Message, foreign_key: :receiver_id, inverse_of: :receiver
   has_many :users_sent, class_name: :User, foreign_key: :receiver_id, source: :receiver, through: :sent_messages
   has_many :users_received, class_name: :User, foreign_key: :sender_id, source: :sender, through: :received_messages
+
+  has_secure_password
+  validates :email, :nickname, presence: true
+  validates :uid, uniqueness: true, if: :uid
+
+  def self.github_authenticate(authorize_params)
+    user = User.find_by(uid: authorize_params.fetch('uid'))
+    return user unless user.nil?
+
+    User.create!(
+      uid: authorize_params.fetch('uid'),
+      email: authorize_params.fetch('info').fetch('email'),
+      nickname: authorize_params.fetch('info').fetch('nickname')
+    )
+  end
 end
