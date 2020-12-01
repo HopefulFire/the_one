74c8f1a6b28117bc4dc705bcf835e1d96d3ee493
Jade Lynn Masker <jadelynn@masker.family>
2020-12-01 14:30:02 -0500

    created pages controller, work on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    return render 'new' unless @user&.authenticate(params[:password])
+
+    session[:id] = @user.id
+    redirect_to root_path
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'pages#home'
+  resources :sessions, only: %i[new create destroy]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
