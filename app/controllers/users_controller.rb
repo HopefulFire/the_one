af08c5f62923700078a89704f3be5722382addce
Jade Lynn Masker <jadelynn@masker.family>
2020-12-01 19:22:08 -0500

    added check authorization for show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :find_user, only: %i[show edit update destroy]
-  before_action :check_authorization, only: %i[edit update destroy]
+  before_action :check_authorization, only: %i[show edit update destroy]
 
   def show; end
 
