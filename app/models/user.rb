0a71453517c21ae0462655e4d80028f2ce8742da
Jade Lynn Masker <jadelynn@masker.family>
2020-12-03 12:59:35 -0500

    beginning of user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :sent_messages, class_name: :Message, foreign_key: :sender_id, inverse_of: :sender
+  has_many :received_messages, class_name: :Message, foreign_key: :receiver_id, inverse_of: :receiver
+
+
   validates :nickname, :provider, :email, :uid, presence: true
   validates :email, uniqueness: true
 
