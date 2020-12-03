b5bad54612fc3618bf56a119628ab5dab908e220
Jade Lynn Masker <jadelynn@masker.family>
2020-12-03 02:03:56 -0500

    work on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    provider = authorize_params['provider']
+    uid = authorize_params['uid']
+    info = authorize_params['info']
+    User.find_or_create_by(uid: uid, provider: provider) do |user|
+      user.email = info['email']
+      case provider
+      when 'google_oauth2'
+        user.nickname = info['nickname']
+        user.firstname = info['first_name']
+        user.lastname = info['last_name']
+      end
+    end
+  end
+
+  def destroy
+    session[:id].delete
+    redirect_to root_path
+  end
+
+  private
+
+  def authorize_params
+    request.env['omniauth.auth']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/sessions/new', to: 'sessions#new', as: 'login'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
