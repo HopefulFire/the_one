4cf6739238760c1516f082fe184e411230016444
Jade Lynn Masker <jadelynn@masker.family>
2020-12-03 12:46:49 -0500

    rails g model Message body:text sender_id:integer:index receiver:integer:index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_100225) do
+ActiveRecord::Schema.define(version: 2020_12_03_174552) do
 
   create_table "identities", force: :cascade do |t|
     t.string "nickname"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2020_12_03_100225) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["uid"], name: "index_identities_on_uid", unique: true
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "sender_id"
+    t.integer "receiver"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["receiver"], name: "index_messages_on_receiver"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
   end
 
   create_table "users", force: :cascade do |t|
