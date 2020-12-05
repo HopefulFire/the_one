5e69ac7551eae829c148f148075dfdb139506ca9
Jade Lynn Masker <jadelynn@masker.family>
2020-12-05 15:37:12 -0500

    work on sessions, almost done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    if params[:provider] == 'none'
+      @user = User.find_by(email: params[:email])
+      return render 'new' unless @user.authenticate(params[:password])
+    end
+    puts 'We dont know what to do here'
+  end
 
   def destroy
     session[:id].delete
-
     redirect_to root_path
   end
 end
