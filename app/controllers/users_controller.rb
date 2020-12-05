be947edf38268bfac20695191e1049d20ce00fcb
Jade Lynn Masker <jadelynn@masker.family>
2020-12-05 14:41:03 -0500

    work on users controller, added provider and uid to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.provider = 'None'
+    @user.uid = SecureRandom.hex
+    render 'new' unless @user.save
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 2020_12_05_174602) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "provider"
+    t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
