5ad794ff6869523eb4aa61789d0cf62a068a0a64
Jade Lynn Masker <jadelynn@masker.family>
2020-12-06 02:59:48 -0500

    finished associations (hopefully) and migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :sent_messages, class_name: :Message, foreign_key: :sender_id, inverse_of: :sender
   has_many :received_messages, class_name: :Message, foreign_key: :reciever_id, inverse_of: :receiver
-  has_many :sent_users, class_name: :User, foreign_key: :reciever_id, source:
+  has_many :sent_users, class_name: :User, foreign_key: :receiver_id, through: :sent_messages, source: :receiver
+  has_many :recieved_users, class_name: :User, foreign_key: :sender_id, through: :received_messages, source: :sender
   has_one :profile, dependent: :destroy, inverse_of: :user
   has_secure_password
 
