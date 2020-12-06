cab084f47cffe588142b6ada12c6162ede122d7d
Jade Lynn Masker <jadelynn@masker.family>
2020-12-05 20:37:11 -0500

    rails g controller Profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user, inverse_of: :profile
 
-  validates :nickname, presence: true
+  validates :nickname, :user_id, presence: true
+  validates :user_id, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/auth/:provider/callback', to: 'sessions#create'
   resources :sessions, only: %i[new create destroy]
-  resources :users
+  resources :users do
+    get '/profile/edit', to: 'profile#edit'
+    patch '/profile', to: 'profiles#update'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
