a0ba730750c173afe81976d90b47ec11bab975e9
Jade Lynn Masker <jadelynn@masker.family>
2020-12-06 03:06:22 -0500

    fixed a misspelling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :sent_messages, class_name: :Message, foreign_key: :sender_id, inverse_of: :sender
-  has_many :received_messages, class_name: :Message, foreign_key: :reciever_id, inverse_of: :receiver
+  has_many :received_messages, class_name: :Message, foreign_key: :receiver_id, inverse_of: :receiver
   has_many :sent_users, class_name: :User, foreign_key: :receiver_id, through: :sent_messages, source: :receiver
   has_many :recieved_users, class_name: :User, foreign_key: :sender_id, through: :received_messages, source: :sender
   has_one :profile, dependent: :destroy, inverse_of: :user
