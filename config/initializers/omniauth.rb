f248d8bb368e4cc4a624279c1727ff2a70e6ffde
Jade Lynn Masker <jadelynn@masker.family>
2020-12-05 19:17:39 -0500

    add google to providers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     Rails.application.credentials.github[:client_id],
     Rails.application.credentials.github[:client_secret]
   )
+  provider(
+    :google_oauth2,
+    Rails.application.credentials.google[:client_id],
+    Rails.application.credentials.google[:client_secret]
+  )
 end
