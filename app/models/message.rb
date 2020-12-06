30630eded7078d8859f727d8461da49056976522
Jade Lynn Masker <jadelynn@masker.family>
2020-12-06 02:47:36 -0500

    work on message associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+  belongs_to :sender, class_name: :User, inverse_of: :sent_messages
+  belongs_to :receiver, class_name: :User, inverse_of: :received_messages
 end
