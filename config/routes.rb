409c88d9d4c0660cd5aadf4ddac4c10da7f64e74
Jade Lynn Masker <jadelynn@masker.family>
2020-12-08 18:57:29 -0500

    rails g controller Messages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[new create destroy]
   resources :chats, only: %i[index show update destroy]
   resources :users do
+    resources :messages, only: %i[index]
     get '/profile/edit', to: 'profiles#edit'
     patch '/profile', to: 'profiles#update'
   end
