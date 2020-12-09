829a89af7aa90b48c4f3ae4414352c21adb62315
Jade Lynn Masker <jadelynn@masker.family>
2020-12-08 19:08:38 -0500

    refactored some code into a partial, created nested resource for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,7 @@
 class MessagesController < ApplicationController
+  def index
+    @user = User.find_by(id: params[:user_id])
+    
+    @messages = Message.where('message.sender_id = ? OR message.receiver_id = ?', @user.id, @user.id)
+  end
 end
