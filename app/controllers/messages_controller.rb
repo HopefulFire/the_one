5c0e3c03d03bd3744d86fb810693396b3b42ad3a
Jade Lynn Masker <jadelynn@masker.family>
2020-12-08 19:09:50 -0500

    made messages nested resource private

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
-    
+    return redirect_to root_path unless @user.id == session[:id]
+
     @messages = Message.where('message.sender_id = ? OR message.receiver_id = ?', @user.id, @user.id)
   end
 end
