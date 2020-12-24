d5d4855f6741e94b22e6ccddf363e24f9659267c
Jade Lynn Masker <jadelynn@masker.family>
2020-12-24 13:18:36 -0500

    fix missing commas

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :sender, class_name: :User, inverse_of: :sent_messages
   belongs_to :receiver, class_name: :User, inverse_of: :received_messages
 
-  scope :sent_and_received_messages ->(user) do
+  scope :sent_and_received_messages, -> (user) do
   	where(
       'sender_id = ? OR receiver_id = ?',
       user.id,
@@ -10,7 +10,7 @@ class Message < ApplicationRecord
     ).order(created_at: :desc)
   end
 
-  scope :chat ->(user, chat_user) do
+  scope :chat, -> (user, chat_user) do
   	where(
       'sender_id = ? AND receiver_id = ? OR sender_id = ? AND receiver_id = ?',
       user.id,
